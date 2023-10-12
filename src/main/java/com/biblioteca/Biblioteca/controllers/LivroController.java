8cac179b77a73b3dd291741867cd3ea36db641e6
Rafael Figueiredo <rafaelferreira-10@hotmail.com>
2023-10-12 17:15:25 -0300

    primeira classe

---
+++ src/main/java/com/biblioteca/Biblioteca/controllers/LivroController.java
@@ -0,0 +1,18 @@
+package com.biblioteca.Biblioteca.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/livros")
+public class LivroController {
+
+    @PostMapping
+    public void cadastrar(@RequestBody String json){
+
+        System.out.println(json);
+
+    }
+}
